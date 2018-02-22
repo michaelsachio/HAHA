46d8b1f012cf087f666ec4dda48b850685c0c560
michaelsachio <michaelsachio@gmail.com>
2018-02-22 21:02:47 +0700

    HAHA

--- 0.js
+++ 0.js
@@ -10,15 +10,3 @@ var y = 12
 console.log (y)
 var z = 12
 console.log (z)
-var x = 12
-console.log (x)
-var y = 12
-console.log (y)
-var z = 12
-console.log (z)
-var x = 12
-console.log (x)
-var y = 12
-console.log (y)
-var z = 12
-console.log (z)
